0bb983f1bb147b84e5d2eaf320826eb1e492b2d1
Keo Coelho de Almeida <keo.almeida@totvs.com.br>
2024-01-17 17:04:17 -0300

    Json - lendo um arquivo

---
+++ JS-123/objetos/json/lendo-um-arquivo/leitura.js
@@ -0,0 +1,8 @@
+// o que é require?
+// função que permite importar um módulo, ou seja, puxa o código de algum outro lugar
+// https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Guide/Modules
+
+const dados = require ('./cliente.json');
+
+console.log(dados);
+console.log (typeof dados);
